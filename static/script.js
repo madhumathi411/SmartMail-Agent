0af7530c10b42316136737a3b6951b97820fdeb0
madhumathi411 <madhumathiganesan11@gmail.com>
2026-09-24 12:57:15 +0530

    Add JavaScript functionality

---
+++ static/script.js
@@ -0,0 +1,15 @@
+function showMessage(message) {
+    alert(message);
+}
+
+function analyzeEmail() {
+    showMessage("AI email analysis will be connected in the next step.");
+}
+
+function generateResponse() {
+    showMessage("AI response generation will be connected soon.");
+}
+
+function approveResponse() {
+    showMessage("Response approved successfully.");
+}
